037c4a2cbd5b8c227d2ac47fc9480a60c79c123f
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-16 20:18:23 +0500

    My Portfolio Page Completely Working

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker , presence: true
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client_api[:publishable_key],
@@ -10,5 +15,9 @@ class Stock < ApplicationRecord
     rescue => exception
       return nil
     end
+  end
+
+  def self.check_existence(ticker_symbol)
+    where(ticker: ticker_symbol).first #same as of Stock.where(ticker: ticker_symbol).first
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def under_stock_limit?
+    stocks.count < 10       #same as of user.stocks.count < 10
+  end
+
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_existence(ticker_symbol)
+    return false unless stock
+    stocks.where(id: stock.id).exists?       #same as of user.stocks.where(id: stock.id).exists?
+  end
+
+  def can_track_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+  end
 end
